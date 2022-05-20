1b15fa7ef996d605ece20d037d174bb1dde2dfaf
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-05-20 08:59:56 -0500

    add: Se agrego el login (interfaz)

--- src/app/components/Login/login.jsx
+++ src/app/components/Login/login.jsx
@@ -1,10 +1,26 @@
 import {React, Component } from "react";
 
+import '../../global/global.css';
+import './login.css';
 
 class Login extends Component{
 
     render(){
-        <h2>Login</h2>
+        return ( 
+            <div className="login__container">
+                
+                <form className="login__form">
+                    <input type="text" className="login__input" placeholder="Ingresa tu correo" />
+                    <input type="password" className="login__input" placeholder="Ingresa tu contraseña" />
+                </form>
+           
+               <div className="login__footer">
+                    <a href="#" className="login__link">Crear una cuenta</a>
+                    <button type="submit" className="login__button">Iniciar Sesión</button>
+               </div>
+
+            </div>
+        )
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import './app/global/global.css';
-import './app/global/normalize.css';
 import App from './App';
 import Navbar from './app/components/Navbar/navbar';
+import Login from './app/components/Login/login';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar></Navbar>
+    <BrowserRouter>
+      <Login/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
