af705dfa6b632c1d9b9f096328832540cc63b8eb
andres <19170163@uttcampus.edu.mx>
2022-07-04 21:38:00 -0500

    Se añadieron las tablas para el control de usuarios y control de blogs

--- src/app/components/Administrator/Blogs/blogs.jsx
+++ src/app/components/Administrator/Blogs/blogs.jsx
@@ -1,9 +1,34 @@
 import {React, Component } from "react";
+import NavbarAdministrator from "../Navbar/navbar";
+
+import './blogs.css';
+
 
 class BlogsAdministrator extends Component{
     render(){
         return (
-            <h1>Control de blogs</h1>
+            <div>
+                <NavbarAdministrator></NavbarAdministrator>
+                <div className="table-center">
+                    <table className="table-border">
+                        <tr>
+                            <th className="table-head">Nombre usuario</th>
+                            <th className="table-head">Correo electronico</th>
+                            <th className="table-head">Estatus</th>
+                        </tr>
+                        <tr>
+                            <td className="table-border">Mr.Bean</td>
+                            <td className="table-border">mr.bean@email.com</td>
+                            <td className="table-border"><input type="checkbox" name="usu_estatus"></input><label className="label-checkbox" for="usu_estatus">Activo</label></td>
+                        </tr>
+                        <tr>
+                            <td className="table-border">PeterParker</td>
+                            <td className="table-border">parker@email.com</td>
+                            <td className="table-border"><input type="checkbox" name="usu_estatus"></input><label className="label-checkbox" for="usu_estatus">Activo</label></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
--- src/app/components/Administrator/Users/users.jsx
+++ src/app/components/Administrator/Users/users.jsx
@@ -1,9 +1,33 @@
 import { React, Component } from "react";
+import NavbarAdministrator from "../Navbar/navbar";
+
+import './users.css';
 
 class UsersAdministrator extends Component{
     render(){
         return (
-            <h1>Control de usuarios</h1>
+            <div>
+                <NavbarAdministrator></NavbarAdministrator>
+                <div className="table-center">
+                    <table className="table-border">
+                        <tr>
+                            <th className="table-head">Nombre usuario</th>
+                            <th className="table-head">Correo electronico</th>
+                            <th className="table-head">Estatus</th>
+                        </tr>
+                        <tr>
+                            <td className="table-border">Mr.Bean</td>
+                            <td className="table-border">mr.bean@email.com</td>
+                            <td className="table-border"><input type="checkbox" name="usu_estatus"></input><label className="label-checkbox" for="usu_estatus">Activo</label></td>
+                        </tr>
+                        <tr>
+                            <td className="table-border">PeterParker</td>
+                            <td className="table-border">parker@email.com</td>
+                            <td className="table-border"><input type="checkbox" name="usu_estatus"></input><label className="label-checkbox" for="usu_estatus">Activo</label></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
